44e8f0dffdde2f1468b6fd2375c22c9dca6f1a62
AmitTayal <amit.tayal.20@gmail.com>
2019-06-08 10:22:47 -0500

    Update startApp.sh
    updated database instance name

--- startApp.sh
+++ startApp.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export NODE_ENV=production
-export DB_PRD_HOST=dellstore2.cohobyukovto.ap-south-1.rds.amazonaws.com
+export DB_PRD_HOST=dellstore2.ca8foft3urki.us-east-1.rds.amazonaws.com
 export DB_PRD_USER=dellstoreaws
 export DB_PRD_PASS=dellstoreaws
 export NODE_HOST=localhost
