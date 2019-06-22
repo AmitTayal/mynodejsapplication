8e5b304219cf312da3bac1980d986c3b1a395552
AmitTayal <amit.tayal.20@gmail.com>
2019-06-22 03:06:45 -0500

    add sudo to startApp.sh

--- startApp.sh
+++ startApp.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-npm install
+sudo npm install
 export NODE_ENV=production
 export DB_PRD_HOST=dellstore2.ca8foft3urki.us-east-1.rds.amazonaws.com
 export DB_PRD_USER=dellstoreaws
 export DB_PRD_PASS=dellstoreaws
 export NODE_HOST=localhost
 export NODE_PORT=8080
-node /myapp/index.js&
+sudo node /myapp/index.js&
 exit 0
